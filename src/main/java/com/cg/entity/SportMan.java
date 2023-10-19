214a7d2d3845be1f16954b54c9bbe1e4e46e0a3f
Ishwar <ishwar.pandurang-mahajan@capgemini.com>
2023-10-19 17:02:41 +0530

    Initial commit

--- src/main/java/com/cg/entity/SportMan.java
+++ src/main/java/com/cg/entity/SportMan.java
@@ -1,5 +1,56 @@
 package com.cg.entity;
 
-public class SportMan {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class SportMan {
+	@Id
+	private int sportMan_id;
+	private String sport_Name;
+	private String sport_Salary;
+
+	public SportMan() {
+		
+	}
+
+	public SportMan(int sportMan_id, String sport_Name, String sport_Salary) {
+		super();
+		this.sportMan_id = sportMan_id;
+		this.sport_Name = sport_Name;
+		this.sport_Salary = sport_Salary;
+	}
+
+	public int getSportMan_id() {
+		return sportMan_id;
+	}
+
+	public void setSportMan_id(int sportMan_id) {
+		this.sportMan_id = sportMan_id;
+	}
+
+	public String getSport_Name() {
+		return sport_Name;
+	}
+
+	public void setSport_Name(String sport_Name) {
+		this.sport_Name = sport_Name;
+	}
+
+	public String getSport_Salary() {
+		return sport_Salary;
+	}
+
+	public void setSportName_Salary(String sport_Salary) {
+		this.sport_Salary = sport_Salary;
+	}
+
+	@Override
+	public String toString() {
+		return "SportMan [sportMan_id=" + sportMan_id + ", sport_Name=" + sport_Name + ", sportName_Salary="
+				+ sport_Salary + "]";
+	}
+	
+	
+	
 }
